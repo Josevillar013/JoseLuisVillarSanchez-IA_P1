6a356ee4b6cf439dedafb3c80c30c2f224f43d59
Josevillar013 <a21110341@ceti.mx>
2025-03-17 17:09:41 -0600

    strings

---
+++ 03_Strings.py
@@ -0,0 +1,8 @@
+mensaje = "Esto es un 'string'"
+mensaje2 = 'Esto es un "string'
+
+# Almacenar la frase literal (con error en comillas)
+mensaje_frase = "print() se utiliza para imprimir valores en la consola.
+
+# Imprimir la frase
+print(mensaje_frase)
