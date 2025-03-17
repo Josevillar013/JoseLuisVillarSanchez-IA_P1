1c9cafc87854c960c55135a9ae274563c2ddc50f
Josevillar013 <a21110341@ceti.mx>
2025-03-17 17:18:23 -0600

    concatenar

--- 04_Concatenar.py
+++ 04_Concatenar.py
@@ -1,4 +1,20 @@
-nombre = "Jose"
-edad = 25
-mensaje = nombre + " tiene " + edad + " años"
+palabra_1 = 'Programación'
+palabra_2 = 'fácil'
+nombre_completo = palabra_1 + ' ' + palabra_2
+print(nombre_completo)
+
+palabra_1 = 'Python'
+palabra_2 = 'es genial'
+mensaje = palabra_1 + ' ' + palabra_2
 print(mensaje)
+
+nombre = 'Jose Luis'
+apellido_1 = 'Villar'
+apellido_2 = 'Sanchez'
+nombre_completo = nombre + ' ' + apellido_1 + ' ' + apellido_2
+print(nombre_completo)
+
+num1 = '101'
+num2 = '150'
+resultado = num1 + num2
+print(resultado)
