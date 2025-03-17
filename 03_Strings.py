c0b32a663c465aa945237086e5892aa034d58277
Josevillar013 <a21110341@ceti.mx>
2025-03-17 17:12:46 -0600

    strings

--- 03_Strings.py
+++ 03_Strings.py
@@ -1,8 +1,9 @@
-mensaje = "Esto es un 'string'"
-mensaje2 = 'Esto es un "string'
+# Definir dos variables con comillas simples y dobles
+mensaje1 = 'Esto es un "string"'
+mensaje2 = "Esto es un 'string'"
 
-# Almacenar la frase literal (con error en comillas)
-mensaje_frase = "print() se utiliza para imprimir valores en la consola.
+# Almacenar la frase literal
+mensaje_frase = "\"print()\" se utiliza para imprimir valores en la consola."
 
 # Imprimir la frase
 print(mensaje_frase)
