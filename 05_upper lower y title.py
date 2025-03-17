51735888e0e130b1b69cf47804a793a94299dd21
Josevillar013 <a21110341@ceti.mx>
2025-03-17 17:31:13 -0600

    upper lower y title

---
+++ 05_upper lower y title.py
@@ -0,0 +1,7 @@
+nombre = "Juan Pérez"
+nombre = nombre.title()
+print(nombre)
+
+# Aquí está el error: intentando usar un método que no existe.
+nombre = nombre.capitalizeFirstLetter()
+print(nombre)
