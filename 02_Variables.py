723d8ee9b0c3fa8777302316a90ba8267fe62a0a
Josevillar013 <a21110341@ceti.mx>
2025-03-17 17:04:58 -0600

    02_variables

---
+++ 02_Variables.py
@@ -0,0 +1,14 @@
+# Primer ejercicio: Guardar un mensaje en una variable
+mensaje = "Bienvenidos al curso de Python"
+
+# Segundo ejercicio: Crear dos variables con números
+numero1 = 11
+numero2 = 13
+
+# Intento de sumar las dos variables y guardar el resultado en una tercera variable
+try:
+    resultado = numero1 + numero2
+    print(resultado)  # Si la suma es exitosa, imprime el resultado
+except:
+    print(mensaje)  # Si hay algún error, imprime el mensaje del primer ejercicio
+
