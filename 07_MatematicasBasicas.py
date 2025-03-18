d6f7fb4ceef20b56d1faf1c2715106e0e88fb801
Josevillar013 <a21110341@ceti.mx>
2025-03-17 18:04:27 -0600

    operaciones basicas

---
+++ 07_MatematicasBasicas.py
@@ -0,0 +1,21 @@
+# Suma con los números 20 y 23
+suma = 20 + 23 + 44  # 20 + 23 + 44 = 87
+
+# Resta con los números 20 y 23
+resta = 20 - 23 - 87  # Error, no da como resultado -87
+
+# Multiplicación con los números 20 y 23
+multiplicacion = 20 * 23 * "a"  # Error, intentamos multiplicar con un valor no numérico
+
+# División con los números 5000 y 230
+division = 5000 / 230  # Error, olvidamos hacer que el resultado sea 10
+
+# Operación con los símbolos aritméticos básicos
+operacion = 10 - 5 * 15 / 17 + 17  # Error, no da el resultado 0
+
+# Imprimir resultados
+print("Suma: ", suma)
+print("Resta: ", resta)
+print("Multiplicación: ", multiplicacion)
+print("División: ", division)
+print("Resultado de la operación: ", operacion)
