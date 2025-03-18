827abf1274b175a12d2243ba2a74615c0c935ecb
Josevillar013 <a21110341@ceti.mx>
2025-03-17 19:49:00 -0600

    listas

--- 10_Listas.py
+++ 10_Listas.py
@@ -1,17 +1,29 @@
-# Lista de colores
+#¿que color esta en la posicion 3 de la lista?
+
 colores = ["rojo", "azul", "verde", "amarillo", "marrón", "lila", "negro", "rosa"]
 
-# Intentamos acceder a una posición incorrecta o fuera del rango
-color_pos_10 = colores[10]  
+color_pos_3 = colores[3]
+print(color_pos_3)
 
-# Crear una lista con los valores en posiciones incorrectas
-nueva_lista = [None] * 5
+#¿en que posicion se encuentra el color "rojo"?
 
-nueva_lista[6] = "uno"  # la lista tiene solo 5 posiciones, de 0 a 4
+pos_rojo = colores.index("rojo")
+print(pos_rojo)
 
-nueva_lista[2] = "dos"
-nueva_lista[1] = "tres"
-nueva_lista[0] = "cuatro"
-nueva_lista[3] = "cinco"
+#¿en que posicion se encuentra el color "rosa"?
+
+pos_rosa = colores.index("rosa")
+print(pos_rosa)
+
+#Para crear una lista que contenga los valores "uno", "dos", "tres", "cuatro" y "cinco" en las posiciones indicadas
+
+nueva_lista = [None] * 5  # Creamos una lista de 5 elementos vacíos
+
+nueva_lista[4] = "uno"
+nueva_lista[1] = "dos"
+nueva_lista[0] = "tres"
+nueva_lista[3] = "cuatro"
+nueva_lista[2] = "cinco"
 
 print(nueva_lista)
+
