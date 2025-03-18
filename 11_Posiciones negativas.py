eb5054e2b529682077e2d599be4737f9d6f78e49
Josevillar013 <a21110341@ceti.mx>
2025-03-17 19:58:59 -0600

    posiciones negativas

---
+++ 11_Posiciones negativas.py
@@ -0,0 +1,8 @@
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
+
+# Accediendo a los colores utilizando índices negativos
+print(colores[-1])  # 'naranja'
+print(colores[-2])  # 'blanco'
+print(colores[-6])  # 'lila'
+print(colores[-4])  # 'amarillo'
+print(colores[-10]) # 'rojo' (esto es el primer elemento, también se puede usar colores[0])
